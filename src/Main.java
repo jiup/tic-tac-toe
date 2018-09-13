c62e23f2b5c5e110e31a81bf8fbfef5a04668fbc
Jiupeng Zhang <jiupeng.zhang@rochester.edu>
2018-09-13 10:25:47 -0400

    Simplify State

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,11 @@ import domain.State;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println((new State(new int[]{
-                -1,0,-1,
-                -1,-1,0,
-                1,1,-1
-        }, 6).getLastPawn()));
+        State s = State.newInstance().setBoard(new int[][]{
+                {-1, 0, -1},
+                {-1, -1, 0},
+                {1, 1, 0}
+        });
+        System.out.println(s);
     }
 }
--- src/domain/State.java
+++ src/domain/State.java
@@ -1,30 +1,55 @@
 package domain;
 
-import java.util.Arrays;
+public class State implements Cloneable {
+    private static final int SIZE = 3;
+    private static final char[] LABELS = {'O', '-', 'X'};
 
-public class State {
-    public static final char[] LABELS = {'O', '-', 'X'};
-    public static final int PLAYER_NUM = 2;
-    public static final int SIZE = 3;
+    private boolean maxTurn = true;
+    private int[][] board = new int[SIZE][SIZE];
+    private int value = 0;
+    private int depth = 0;
 
-    private int[] costs = new int[PLAYER_NUM];
-    private int[] board = new int[SIZE * SIZE];
-    private int lastStep = -1;
-
-    public State() {
+    public static State newInstance() {
+        return new State();
     }
 
-    public State(int[] board, int lastStep) {
-        this.board = Arrays.copyOf(board, SIZE * SIZE);
-        this.lastStep = lastStep;
+    private State() {
     }
 
-    public int[] getLastStep() {
-        return lastStep < 0 ? null : new int[]{lastStep / SIZE, lastStep % SIZE};
+    public boolean isMaxTurn() {
+        return maxTurn;
     }
 
-    public char getLastPawn() {
-        return lastStep < 0 ? ' ' : LABELS[board[lastStep] + 1];
+    public int[][] getBoard() {
+        return board;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public int getDepth() {
+        return depth;
+    }
+
+    public State flipTurn() {
+        this.maxTurn = !maxTurn;
+        return this;
+    }
+
+    public State setBoard(int[][] board) {
+        this.board = board;
+        return this;
+    }
+
+    public State setValue(int value) {
+        this.value = value;
+        return this;
+    }
+
+    public State setDepth(int depth) {
+        this.depth = depth;
+        return this;
     }
 
     public boolean isTerminal() {
@@ -32,29 +57,33 @@ public class State {
         for (int i = 0; i < SIZE; i++) {
             int rowSum = 0, colSum = 0;
             for (int j = 0; j < SIZE; j++) {
-                if (Math.abs(rowSum += board(i, j)) == SIZE
-                        || Math.abs(colSum += board(j, i)) == SIZE) {
+                if (Math.abs(rowSum += board[i][j]) == SIZE
+                        || Math.abs(colSum += board[j][i]) == SIZE) {
                     return true;
                 }
             }
-            if (Math.abs(bSlashSum += board(i, i)) == SIZE
-                    || Math.abs(slashSum += board(i, SIZE - 1 - i)) == SIZE) {
+            if (Math.abs(bSlashSum += board[i][i]) == SIZE
+                    || Math.abs(slashSum += board[i][SIZE - 1 - i]) == SIZE) {
                 return true;
             }
         }
         return false;
     }
 
-    public String toString() {
-        StringBuilder builder = new StringBuilder();
-        for (int i = 0; i < board.length; i++) {
-            builder.append(LABELS[board[i] + 1]).append(" ")
-                    .append((i + 1) % SIZE == 0 ? "\n" : "");
-        }
-        return builder.toString();
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
     }
 
-    private int board(int i, int j) {
-        return board[i * SIZE + j];
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                builder.append(LABELS[board[i][j] + 1]).append(" ");
+            }
+            builder.append("\n");
+        }
+        return builder.toString();
     }
 }
