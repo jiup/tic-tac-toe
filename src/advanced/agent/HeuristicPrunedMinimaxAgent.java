6207e9858a2776af7b7950978077c10b130b9a68
Jiupeng Zhang <jiupeng.zhang@rochester.edu>
2018-09-28 13:22:16 -0400

    Bugfix: check judge mistaken Add Gekun's code

--- src/advanced/agent/HeuristicPrunedMinimaxAgent.java
+++ src/advanced/agent/HeuristicPrunedMinimaxAgent.java
@@ -71,4 +71,122 @@ public class HeuristicPrunedMinimaxAgent implements Agent {
 
         return grade;
     }
+
+
+//    /**
+//     * A private function to see if a sub-board(3X3) is a restricted board for a player.
+//     *
+//     * @param An array of integer represents a sub-board(3X3), and current player(true --> max, false --> min)
+//     * @return An integer: -1 -- opponent is going to win regardless of whether or not
+//     * current player is going to win; 1 --- current player is going to win and opponent
+//     * is not going to win; 2 --- none of players are going to win.
+//     *
+//     */
+//    private int restrictedOrNot(int[] SubBoard, boolean player){
+//        if (player == true){ // currently max turn
+//            if(SubBoard[0]+SubBoard[1]+SubBoard[2] == -2 || SubBoard[3]+SubBoard[4]+SubBoard[5] == -2
+//                    || SubBoard[6]+SubBoard[7]+SubBoard[8] == -2 || SubBoard[0]+SubBoard[3]+SubBoard[6] == -2
+//                    || SubBoard[1]+SubBoard[5]+SubBoard[7] == -2 || SubBoard[2]+SubBoard[5]+SubBoard[8] == -2
+//                    || SubBoard[0]+SubBoard[4]+SubBoard[8] == -2 || SubBoard[2]+SubBoard[4]+SubBoard[6] == -2){ // opponent(min) takes control
+//                return -1;
+//            } else if(SubBoard[0]+SubBoard[1]+SubBoard[2] == 2 || SubBoard[3]+SubBoard[4]+SubBoard[5] == 2
+//                    || SubBoard[6]+SubBoard[7]+SubBoard[8] == 2 || SubBoard[0]+SubBoard[3]+SubBoard[6] == 2
+//                    || SubBoard[1]+SubBoard[5]+SubBoard[7] == 2 || SubBoard[2]+SubBoard[5]+SubBoard[8] == 2
+//                    || SubBoard[0]+SubBoard[4]+SubBoard[8] == 2 || SubBoard[2]+SubBoard[4]+SubBoard[6] == 2){ // itself(max) takes control
+//                return 1;
+//            }
+//        } else{ // currently min turn
+//            if(SubBoard[0]+SubBoard[1]+SubBoard[2] == -2 || SubBoard[3]+SubBoard[4]+SubBoard[5] == -2
+//                    || SubBoard[6]+SubBoard[7]+SubBoard[8] == -2 || SubBoard[0]+SubBoard[3]+SubBoard[6] == -2
+//                    || SubBoard[1]+SubBoard[5]+SubBoard[7] == -2 || SubBoard[2]+SubBoard[5]+SubBoard[8] == -2
+//                    || SubBoard[0]+SubBoard[4]+SubBoard[8] == -2 || SubBoard[2]+SubBoard[4]+SubBoard[6] == -2){ // itself(min) takes control
+//                return 1;
+//            } else if(SubBoard[0]+SubBoard[1]+SubBoard[2] == 2 || SubBoard[3]+SubBoard[4]+SubBoard[5] == 2
+//                    || SubBoard[6]+SubBoard[7]+SubBoard[8] == 2 || SubBoard[0]+SubBoard[3]+SubBoard[6] == 2
+//                    || SubBoard[1]+SubBoard[5]+SubBoard[7] == 2 || SubBoard[2]+SubBoard[5]+SubBoard[8] == 2
+//                    || SubBoard[0]+SubBoard[4]+SubBoard[8] == 2 || SubBoard[2]+SubBoard[4]+SubBoard[6] == 2){ // opponent(max) takes control
+//                return -1;
+//            }
+//        }
+//        return 2;
+//    }
+//
+//    /**
+//     * A private function to count the number of opponent in a sub-board(3X3)
+//     *
+//     * @param An array of integer represents a sub-board, and current player(true --> max, false --> min)
+//     * @return An integer that represents the number of opponents in a sub-board(3X3)
+//     *
+//     */
+//    private int opponentNumber(int[] SubBoard, boolean player){
+//        int playerInt = 0;
+//        if (player == true){ // max
+//            playerInt = 1;
+//        } else{ // min
+//            playerInt = -1;
+//        }
+//        int counter = 0;
+//        for(int i=0; i<9; i++){
+//            if(SubBoard[i] != playerInt) {
+//                counter ++;
+//            }
+//        }
+//        return counter;
+//    }
+//
+//
+//    /**
+//     * A private function that give an estimation of value for one location in a sub-board(3X3)
+//     *
+//     * @param An array of integer represents a sub-board, and current player(true --> max,
+//     * 		false --> min), an integer represents specific place in the sub-board.
+//     * @return An integer that represents the value of a place in a sub-board(3X3)
+//     *
+//     */
+//    private int valueOfPlace(int[] subBoard, boolean player, int place){
+//        // currently simple version
+//        if(place == 5 && subBoard[4] == 0) {
+//            return 3;
+//        } else if((place == 1 && subBoard[0] == 0) || (place == 3 && subBoard[2] == 0) ||
+//                (place == 7 && subBoard[6] == 0) || (place == 9 && subBoard[8] == 0)) {
+//            return 2;
+//        }else if((place == 2 && subBoard[1] == 0) || (place == 4 && subBoard[3] == 0) ||
+//                (place == 6 && subBoard[5] == 0) || (place == 8 && subBoard[7] == 0)) {
+//            return 1;
+//        }
+//        return 0;
+//    }
+//
+//    // helper function smae as pieceValue in state.java
+//    int pieceValue2(char c) {
+//        if (c == 'X') return 1;
+//        if (c == 'O') return -1;
+//        return 0;
+//    }
+//
+//
+//
+//    private int heuristic2(State state) {
+//        int Score = 0;
+//        for(int i=1; i<10; i++){
+//            int[] subBoard = new int[9];
+//            int tempS = 0; // temporary int that keep track of highest score of 9 sub-boards
+//            int tempPlaceMax = 0; // temporary int that keep track of highest score of a place in a sub-board
+//            for(int j=1; j<10; j++){ // go through 9 sub-boards to get score
+//                int P = pieceValue2( state.board(i, j) );
+//                subBoard[j-1] = P;
+//                int placeValue = valueOfPlace(subBoard, state.isMaxTurn(), j);
+//                if (placeValue > tempPlaceMax){
+//                    tempPlaceMax = placeValue;
+//                }
+//            }
+//            tempS += restrictedOrNot(subBoard, state.isMaxTurn())*1000;
+//            tempS += opponentNumber(subBoard, state.isMaxTurn())*100;
+//            tempS += tempPlaceMax;
+//            if(tempS > Score){
+//                Score = tempS;
+//            }
+//        }
+//        return Score;
+//    }
 }
--- src/advanced/domain/AdvanceState.java
+++ src/advanced/domain/AdvanceState.java
@@ -27,51 +27,53 @@ public class AdvanceState extends State {
                 return this;
             }
         }
-        if (Math.abs(hSum) == 2)
+        if (Math.abs(hSum) == 2) {
             if (maxTurn) maxCheck[boardIndex - 1]++;
             else minCheck[boardIndex - 1]++;
-
-        if (Math.abs(vSum) == 2)
+        }
+        if (Math.abs(vSum) == 2) {
             if (maxTurn) maxCheck[boardIndex - 1]++;
             else minCheck[boardIndex - 1]++;
-
-        if ((maxTurn && hSum == -1) || (!maxTurn && hSum == 1))
+        }
+        if ((maxTurn && hSum == -1) || (!maxTurn && hSum == 1)) {
             if (maxTurn) minCheck[boardIndex - 1]--;
             else maxCheck[boardIndex - 1]--;
-
-        if ((maxTurn && vSum == -1) || (!maxTurn && vSum == 1))
+        }
+        if ((maxTurn && vSum == -1) || (!maxTurn && vSum == 1)) {
             if (maxTurn) minCheck[boardIndex - 1]--;
             else maxCheck[boardIndex - 1]--;
-
-        if (i == j || i + j == 2) {
+        }
+        if (i == j || i + j == 4) {
             for (int k = 1; k <= 3; k++) {
                 if (Math.abs(d1Sum += pieceValue(board(boardIndex, k, k))) == 3) {
                     value = maxTurn ? Integer.MAX_VALUE : Integer.MIN_VALUE;
                     terminal = true;
                     return this;
                 }
-                if (Math.abs(d2Sum += pieceValue(board(boardIndex, k, 2 - k))) == 3) {
+                if (Math.abs(d2Sum += pieceValue(board(boardIndex, k, 4 - k))) == 3) {
                     value = maxTurn ? Integer.MAX_VALUE : Integer.MIN_VALUE;
                     terminal = true;
                     return this;
                 }
             }
-            if (Math.abs(d1Sum) == 2)
+            if (i == 1 && j == 3 || i == 3 && j == 1) d1Sum = 0;
+            if (i == 1 && j == 1 || i == 3 && j == 3) d2Sum = 0;
+            if (Math.abs(d1Sum) == 2) {
                 if (maxTurn) maxCheck[boardIndex - 1]++;
                 else minCheck[boardIndex - 1]++;
-
-            if (Math.abs(d2Sum) == 2)
+            }
+            if (Math.abs(d2Sum) == 2) {
                 if (maxTurn) maxCheck[boardIndex - 1]++;
                 else minCheck[boardIndex - 1]++;
-
-            if ((maxTurn && d1Sum == -1) || (!maxTurn && d1Sum == 1))
+            }
+            if ((maxTurn && d1Sum == -1) || (!maxTurn && d1Sum == 1)) {
                 if (maxTurn) minCheck[boardIndex - 1]--;
                 else maxCheck[boardIndex - 1]--;
-
-            if ((maxTurn && d2Sum == -1) || (!maxTurn && d2Sum == 1))
+            }
+            if ((maxTurn && d2Sum == -1) || (!maxTurn && d2Sum == 1)) {
                 if (maxTurn) minCheck[boardIndex - 1]--;
                 else maxCheck[boardIndex - 1]--;
-
+            }
         }
         boolean boardFull = true;
         for (int c : zoneCount) {
